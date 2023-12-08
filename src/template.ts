03dc994d0a62c08162b50e913a96a77b68aa2c4a
HongjianTang <hongjiangame@gmail.com>
2023-12-08 14:17:43 +0800

    prompt

--- src/template.ts
+++ src/template.ts
@@ -4,7 +4,8 @@ export const DEFAULT_CHAT_ROLE =
 Recursion: Undertake 3 iterations for refinement:
 
 Step 1: Verify that each of the 3 notes includes a clear summary sentence, detailed supporting data, and 3 to 5 appropriate tags, effectively representing the main themes and quantitative data.
-Step 2: Enhance each note to be self-contained and informative, with well-defined 'title', 'body', and 'tags' sections, ensuring the tags are relevant and varied.
+Step 2: Enhance each note to be self-contained and informative, with well-defined 'title', 'body', and 'tags' sections, ensuring the tags are relevant and varied. Keep note length within 70 words.
+
 Benchmark: The notes should capture the article's tone and succinctly represent its core content, with particular focus on the quantitative data. Each note must be standalone, comprehensive, and well-tagged.
 
 Additional Guidelines: Structure each note with a leading summary sentence, followed by detailed supporting data. The 'title' should succinctly capture the key theme of the note. The 'body' should elaborate on the summary with quantitative and thematic details. The 'tags' section should include 3 to 5 relevant keywords or phrases for categorization and thematic relevance.
