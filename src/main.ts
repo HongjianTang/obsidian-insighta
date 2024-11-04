d969f93c1ba06e7b964be10a7363dc37ac158c68
HongjianTang <wanquanbuhuiyingwen@gmail.com>
2024-11-04 18:37:33 +0800

    refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,22 @@ import { ViewManager } from "src/view-manager";
 import { ChatGPT } from 'src/api';
 import { Embed } from 'src/embed';
 
-enum InputType {
-	SelectedArea,
-	Content
+enum InputMode {
+	SelectedText,
+	FullContent
 }
 
 export default class InsightAPlugin extends Plugin {
 	settings: InsightASettings;
-	embed: Embed;
+	embedManager: Embed;
 	viewManager = new ViewManager(this.app);
 
 	async onload() {
 		await this.loadSettings();
 		process.env.OPENAI_API_KEY = this.settings.commandOption.openai_key;
-		this.embed = new Embed(this.app, this.viewManager, this.settings);
+		this.embedManager = new Embed(this.app, this.viewManager, this.settings);
 
-		this.registerCommands();
+		this.registerPluginCommands();
 		this.addSettingTab(new InsightASettingTab(this.app, this));
 	}
 
@@ -32,34 +32,34 @@ export default class InsightAPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	saveSettingsInstant() {
+	saveSettingsNow() {
 		this.saveData(this.settings);
 	}
 
 	onunload() {}
 
-	private registerCommands() {
+	private registerPluginCommands() {
 		this.addCommand({
-			id: 'create-atomic-notes-selected',
-			name: 'Create atomic notes from Selected Area',
-			callback: () => this.runExtractNotes(InputType.SelectedArea)
+			id: 'create-notes-from-selection',
+			name: 'Create Notes from Selected Text',
+			callback: () => this.extractNotes(InputMode.SelectedText)
 		});
 		this.addCommand({
-			id: 'create-atomic-notes-content',
-			name: 'Create atomic notes from Note Content',
-			callback: () => this.runExtractNotes(InputType.Content)
+			id: 'create-notes-from-content',
+			name: 'Create Notes from Full Content',
+			callback: () => this.extractNotes(InputMode.FullContent)
 		});
 		this.addCommand({
-			id: 'update-moc',
-			name: 'Update moc',
-			callback: () => this.updateMoc()
+			id: 'update-map-of-content',
+			name: 'Update Map of Content',
+			callback: () => this.updateMapOfContent()
 		});
 	}
 
-	private async runExtractNotes(inputType: InputType) {
-		const loadingNotice = this.createLoadingNotice(`${this.manifest.name}: Processing..`);
+	private async extractNotes(inputMode: InputMode) {
+		const loadingNotice = this.showLoadingNotice(`${this.manifest.name}: Processing...`);
 		try {
-			await this.extractNotes(inputType);
+			await this.processNotes(inputMode);
 		} catch (err) {
 			console.error(err);
 		} finally {
@@ -67,54 +67,54 @@ export default class InsightAPlugin extends Plugin {
 		}
 	}
 
-	private async extractNotes(inputType: InputType) {
-		if (!this.isApiKeyValid()) {
+	private async processNotes(inputMode: InputMode) {
+		if (!this.isApiKeyAvailable()) {
 			return;
 		}
 
-		const input = await this.getInput(inputType);
-		if (!input) {
-			new Notice(`⛔ ${this.manifest.name}: no input data`);
+		const noteInput = await this.getNoteInput(inputMode);
+		if (!noteInput) {
+			new Notice(`⛔ ${this.manifest.name}: No input data`);
 			return;
 		}
 
-		const filename = await this.viewManager.getTitle() ?? "Untitled";
-		const userPrompt = this.formatUserPrompt(input);
-		const systemPrompt = this.formatSystemPrompt();
+		const noteTitle = await this.viewManager.getTitle() ?? "Untitled";
+		const userPrompt = this.generateUserPrompt(noteInput);
+		const systemPrompt = this.generateSystemPrompt();
 
 		try {
-			const noteArray = await this.getNotesFromApi(systemPrompt, userPrompt);
-			await this.createNotes(noteArray, filename);
-			new Notice(`✅ ${this.manifest.name}: finish`);
+			const notesArray = await this.fetchNotesFromApi(systemPrompt, userPrompt);
+			await this.createNotesFromArray(notesArray, noteTitle);
+			new Notice(`✅ ${this.manifest.name}: Finished`);
 		} catch (error) {
 			console.error(error);
 			new Notice(`⛔ ${this.manifest.name}: Failed to extract notes`);
 		}
 	}
 
-	private isApiKeyValid(): boolean {
+	private isApiKeyAvailable(): boolean {
 		if (!process.env.OPENAI_API_KEY) {
-			new Notice(`⛔ ${this.manifest.name}: You should input your API Key`);
+			new Notice(`⛔ ${this.manifest.name}: You need to input your API Key`);
 			return false;
 		}
 		return true;
 	}
 
-	private async getInput(inputType: InputType): Promise<string | null> {
-		if (inputType === InputType.SelectedArea) {
+	private async getNoteInput(inputMode: InputMode): Promise<string | null> {
+		if (inputMode === InputMode.SelectedText) {
 			return await this.viewManager.getSelection();
-		} else if (inputType === InputType.Content) {
+		} else if (inputMode === InputMode.FullContent) {
 			return await this.viewManager.getContent();
 		}
 		return null;
 	}
 
-	private formatUserPrompt(input: string): string {
+	private generateUserPrompt(input: string): string {
 		let userPrompt = DEFAULT_PROMPT_TEMPLATE;
 		return userPrompt.replace('{{input}}', input);
 	}
 
-	private formatSystemPrompt(): string {
+	private generateSystemPrompt(): string {
 		const { system_role, notes_quantity, tags_quantity, language_option, specific_language, properties } = this.settings.commandOption;
 		return system_role
 			.replace(/{{number_of_notes}}/g, notes_quantity.toString())
@@ -123,28 +123,28 @@ export default class InsightAPlugin extends Plugin {
 			.replace(/{{properties}}/g, properties);
 	}
 
-	private async getNotesFromApi(systemPrompt: string, userPrompt: string): Promise<any[]> {
+	private async fetchNotesFromApi(systemPrompt: string, userPrompt: string): Promise<any[]> {
 		let noteJsonString = await ChatGPT.callAPI(systemPrompt, userPrompt, this.settings.commandOption.llm_model);
 		noteJsonString = noteJsonString.replace(/```json/g, "").replace(/```/g, "");
-		noteJsonString = this.convertToJsonArray(noteJsonString);
+		noteJsonString = this.convertStringToJsonArray(noteJsonString);
 		console.log(`noteJsonString: ${noteJsonString}`);
-		let noteArray;
+		let notesArray;
 		try {
-			noteArray = JSON.parse(noteJsonString);
+			notesArray = JSON.parse(noteJsonString);
 		} catch (error) {
 			throw new Error("Invalid JSON format");
 		}
-		if (!Array.isArray(noteArray)) {
-			new Notice(`⛔ return json is not array`);
-			return [noteArray];
+		if (!Array.isArray(notesArray)) {
+			new Notice(`⛔ Returned JSON is not an array`);
+			return [notesArray];
 		}
-		return noteArray;
+		return notesArray;
 	}
 
-	private async createNotes(noteArray: any[], filename: string) {
-		for (const note of noteArray) {
-			const processedTags = this.processTags(note.tags);
-			const noteContent = this.generateNoteContent(note, filename, processedTags);
+	private async createNotesFromArray(notesArray: any[], title: string) {
+		for (const note of notesArray) {
+			const tags = this.formatTags(note.tags);
+			const noteContent = this.buildNoteContent(note, title, tags);
 			const notePath = `${this.settings.commandOption.generated_notes_location}/${note.title}.md`;
 			try {
 				await this.app.vault.create(notePath, noteContent);
@@ -155,32 +155,32 @@ export default class InsightAPlugin extends Plugin {
 		}
 	}
 
-	private processTags(tags: string[]): string {
+	private formatTags(tags: string[]): string {
 		return tags.map(tag => tag.replace(/ /g, "_").replace("#", "")).join(', ');
 	}
 
-	private generateNoteContent(note: any, filename: string, tags: string): string {
-		return `---\nsource: "[[${filename}]]"\ntags: ${tags}\n---\n${note.body}`;
+	private buildNoteContent(note: any, title: string, tags: string): string {
+		return `---\nsource: "[[${title}]]"\ntags: ${tags}\n---\n${note.body}`;
 	}
 
-	private async updateMoc() {
-		if (!this.isApiKeyValid()) {
+	private async updateMapOfContent() {
+		if (!this.isApiKeyAvailable()) {
 			return;
 		}
 
-		const fileName = await this.viewManager.getTitle();
-		if (!fileName) {
-			new Notice("⛔ Can't get title");
+		const noteTitle = await this.viewManager.getTitle();
+		if (!noteTitle) {
+			new Notice("⛔ Unable to retrieve title");
 			return;
 		}
 
-		await this.embed.saveEmbeddings();
-		await this.embed.searchRelatedNotes(fileName, fileName);
-		new Notice(`✅ ${this.manifest.name}: finish`);
+		await this.embedManager.saveEmbeddings();
+		await this.embedManager.searchRelatedNotes(noteTitle, noteTitle);
+		new Notice(`✅ ${this.manifest.name}: Finished`);
 	}
 
-	private createLoadingNotice(text: string, number = 100000): Notice {
-		const notice = new Notice('', number);
+	private showLoadingNotice(text: string, duration = 100000): Notice {
+		const notice = new Notice('', duration);
 		const loadingContainer = document.createElement('div');
 		loadingContainer.addClass('loading-container');
 
@@ -198,13 +198,11 @@ export default class InsightAPlugin extends Plugin {
 		return notice;
 	}
 
-	private convertToJsonArray(str: string): string {
+	private convertStringToJsonArray(str: string): string {
 		try {
 			JSON.parse(str);
 			return str;
-		} catch (error) {
-			// Not a valid JSON array, proceed with conversion
-		}
+		} catch (error) {}
 
 		let parts = str.split('}').filter(part => part.trim() !== '');
 		let jsonParts = parts.map(part => {
